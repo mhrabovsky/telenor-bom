555e236b9cb5cbcafab46d96a18c4e1a9f3cd0ad
Hrabovsky Marián <hrabocsu@caesar.elte.hu>
2016-10-12 11:27:09 +0200

    törlés

--- BO_MAPPINGS/00_INITIALIZATION/030_PROC_DROP_INDEX.sql
+++
@@ -1,9 +0,0 @@
-DELIMITER $$
-CREATE PROCEDURE DropIndex(in theIndexName varchar(128), in theTable varchar(128) )
-BEGIN
- IF((SELECT COUNT(*) AS index_exists FROM information_schema.statistics WHERE TABLE_SCHEMA = DATABASE() and table_name = theTable AND index_name = theIndexName)  = 1) THEN
-   SET @s = CONCAT('ALTER TABLE  ' , theTable , ' DROP INDEX ' , theIndexName);
-   PREPARE stmt FROM @s;
-   EXECUTE stmt;
- END IF;
-END $$
